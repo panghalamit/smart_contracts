a9630061e4750a077225ebfbfa75f45608790274
panghalamit <panghalamit1892@gmail.com>
2018-06-27 16:24:06 -0400

    events added for frontend interaction

--- jointaccount/JointAccount.sol
+++ jointaccount/JointAccount.sol
@@ -1,6 +1,18 @@
 pragma solidity ^0.4.0;
 
 contract JointOwnableAccount {
+    
+    //Add Events to interact with frontend : self-explanatory
+    
+    event OwnershipChanged(address old_owner, address new_owner);
+    event RequirementChanged(uint old_req, uint new_req);
+    event PartnerAdded(address new_partner);
+    event PartnerRemoved(address old_partner);
+    event Withdrawal(uint amount, uint remaining_balance, address beneficiary);
+    event WithdrawalRequest(uint amount, address requestor);
+    event ApprovalConfirmation(uint req_id, address approver);
+    event ApprovalRevocation(uint req_id, address revoker);
+    
     // balance of the account
     uint256 public balance = 100000;
     
@@ -84,6 +96,7 @@ contract JointOwnableAccount {
         for(uint i=0; i<_partners.length; i++) {
             partners.push(_partners[i]);
             isPartner[_partners[i]]=true;
+            emit PartnerAdded(_partners[i]);
         }
     }
     
@@ -97,6 +110,7 @@ contract JointOwnableAccount {
         
     // function to change requirement , only owner can access    
     function changeRequirement(uint num_signatures) public ownerOnly validateRequirement(partners.length, num_signatures){
+        emit RequirementChanged(sig_requirement, num_signatures);
         sig_requirement=num_signatures;
     }
     
@@ -107,6 +121,7 @@ contract JointOwnableAccount {
                 partners[i] = owner;
                 owner = newOwner;
                 partners[0] = owner;
+                emit OwnershipChanged(partners[i], newOwner);
                 break;
             }
         }
@@ -130,6 +145,7 @@ contract JointOwnableAccount {
     // function to add a new secondary partner : only owner can access
     function addNewPartner(address partner) public ownerOnly notAlreadyPartner(partner){
         partners.push(partner);
+        emit PartnerAdded(partner);
         isPartner[partner] = true;
     }
     
@@ -139,6 +155,7 @@ contract JointOwnableAccount {
         for (uint i=1; i<partners.length - 1; i++)
             if (partners[i] == partner) {
                 partners[i] = partners[partners.length - 1];
+                emit PartnerRemoved(partner);
                 break;
             }
         partners.length -= 1;
@@ -150,6 +167,8 @@ contract JointOwnableAccount {
         balance -= amount;
         if(!msg.sender.send(amount)) {
             balance += amount;
+        } else {
+            emit Withdrawal(amount, balance, msg.sender);
         }
     }
     
@@ -167,11 +186,13 @@ contract JointOwnableAccount {
     // function to approve a withdraw request with a given id : only partners can access
     function approveWithdrawRequest(uint wr_id) checkPartnership Exists(wr_id) notExecuted(wr_id) public {
         transactionsApprovals[wr_id][msg.sender] = true;
+        emit ApprovalConfirmation(wr_id, msg.sender);
     } 
     
     // function to revoke a withdraw request with a given id : only partners can access
     function revokeWithdrawRequest(uint wr_id) checkPartnership Exists(wr_id) notExecuted(wr_id) public {
         transactionsApprovals[wr_id][msg.sender] = false;
+        emit ApprovalRevocation(wr_id, msg.sender);
     }
     
     // internal function to add request to pending list
@@ -182,6 +203,7 @@ contract JointOwnableAccount {
             amount: value,
             executed: false
         });
+        emit WithdrawalRequest(value, recipient);
         transactions += 1;
     }
     
@@ -193,6 +215,8 @@ contract JointOwnableAccount {
             pendingRequests[wr_id].executed=true;
             if(!pendingRequests[wr_id].beneficiary.send(pendingRequests[wr_id].amount)) {
                 balance+=pendingRequests[wr_id].amount;
+            } else {
+                emit Withdrawal(pendingRequests[wr_id].amount, balance, pendingRequests[wr_id].beneficiary);
             }
         }
     }
